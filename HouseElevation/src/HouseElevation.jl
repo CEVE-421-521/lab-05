ff38dac1b921ce3df714dd454396d9db64d85764
James Doss-Gollin <jdossgollin@rice.edu>
2024-02-16 10:41:19 -0600

    looks ready

--- HouseElevation/src/HouseElevation.jl
+++ HouseElevation/src/HouseElevation.jl
@@ -3,6 +3,7 @@ module HouseElevation
 include("house.jl")
 include("lsl.jl")
 include("core.jl")
+include("run_sim.jl")
 
 export DepthDamageFunction,
     House, Oddo17SLR, elevation_cost, ModelParams, SOW, Action, run_sim
